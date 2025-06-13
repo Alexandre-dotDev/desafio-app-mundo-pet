c904541f1a42c39f28b3be2b68d492a029ce5c75
Alexandre-dotDev <alexandre.centro@yahoo.com.br>
2025-06-12 23:02:51 -0300

    Add pacote de dev configs

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./index.css";
+
+console.log("App carregado");
+
+// if (module.hot) {
+//   module.hot.accept();
+// }
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+
+  entry: "./src/index.js",
+
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/imagens/[name][ext]",
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/assets"),
+          to: "assets",
+          noErrorOnMissing: true,
+        },
+      ],
+    }),
+  ],
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 3000,
+    open: true,
+    hot: true,
+    watchFiles: ["src/**/*"],
+  },
+};
